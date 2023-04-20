18257a597be87640599c22ae15af3b754d15ac8a
GDJ <GDJ@DESKTOP-BECS0RM>
2023-04-20 15:07:42 +0900

    02_ri add message to Main

--- jdbcTest3/src/com/jdbc/run/Main.java
+++ jdbcTest3/src/com/jdbc/run/Main.java
@@ -5,8 +5,7 @@ import com.jdbc.controller.ControllerImpl;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new ControllerImpl().mainMenu();
+		System.out.println("내가 추가한 것");
 	}
-
 }
